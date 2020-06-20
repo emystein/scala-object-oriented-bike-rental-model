1505978ed5b674f9192816218e0a218e83457f8f
emystein <emystein@gmail.com>
2020-06-20 11:00:03 -0300

    BikeRepository trait.

---
+++ src/main/scala/ar/com/flow/bikerental/model/BikeRepository.scala
@@ -0,0 +1,8 @@
+package ar.com.flow.bikerental.model
+
+trait BikeRepository {
+  def save(bike: Bike)
+  def getBySerialNumber(serialNumber: String): Option[Bike]
+  def getAll(): Iterable[Bike]
+  def delete(bike: Bike)
+}
