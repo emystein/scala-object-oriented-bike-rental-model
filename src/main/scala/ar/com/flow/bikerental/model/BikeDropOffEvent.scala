543c6577bd69307ecdf126039b67cca0fa6cfd37
Emiliano Menéndez <emystein@gmail.com>
2020-06-20 10:28:58 -0300

    Moved classes into Trip.scala

--- src/main/scala/ar/com/flow/bikerental/model/BikeDropOffEvent.scala
+++
@@ -1,5 +0,0 @@
-package ar.com.flow.bikerental.model
-
-import java.time.LocalDateTime
-
-case class BikeDropOffEvent(user: User, bike: Bike, timestamp: LocalDateTime) extends BikeEvent
--- src/main/scala/ar/com/flow/bikerental/model/Trip.scala
+++ src/main/scala/ar/com/flow/bikerental/model/Trip.scala
@@ -1,3 +1,36 @@
 package ar.com.flow.bikerental.model
 
+import java.time.{DayOfWeek, Duration, LocalDateTime}
+import java.time.LocalDateTime.now
+
+import ar.com.flow.bikerental.model.token.{ConsumedRentToken, ReservedRentToken}
+
+trait BikeEvent {
+  def user: User
+  def bike: Bike
+  def timestamp: LocalDateTime
+}
+
+case class BikePickUpEvent(bike: Bike, reservedToken: ReservedRentToken) extends BikeEvent {
+  val consumedToken: ConsumedRentToken = reservedToken.consume
+  val user = consumedToken.owner
+  // TODO convert to val
+  var timestamp = now
+}
+
+case class BikeDropOffEvent(user: User, bike: Bike, timestamp: LocalDateTime) extends BikeEvent
+
 case class Trip(pickUp: BikePickUpEvent)
+
+class FinishedTrip(var bikePickup: BikePickUpEvent, val dropOffTimestamp: LocalDateTime) {
+  val user = bikePickup.user
+  val bike = bikePickup.bike
+  val bikeDropOff = BikeDropOffEvent(bikePickup.user, bikePickup.bike, dropOffTimestamp)
+
+  def duration: Duration = Duration.between(bikePickup.timestamp, bikeDropOff.timestamp)
+
+  def endDayOfWeek: DayOfWeek = bikeDropOff.timestamp.getDayOfWeek
+
+  def hasLastedMoreThan(duration: Duration): Boolean = this.duration.compareTo(duration) > 0
+}
+
