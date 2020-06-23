4a6d105469396cf235cb1e4043ac8acd79bd1a89
Emiliano Menéndez <emystein@gmail.com>
2020-06-22 23:01:13 -0300

    Clean BikeAnchorage up.

--- src/main/scala/ar/com/flow/bikerental/model/BikeAnchorage.scala
+++ src/main/scala/ar/com/flow/bikerental/model/BikeAnchorage.scala
@@ -1,6 +1,6 @@
 package ar.com.flow.bikerental.model
 
-import ar.com.flow.bikerental.model.token.{ReservedRentToken, Token}
+import ar.com.flow.bikerental.model.token.ReservedRentToken
 import ar.com.flow.bikerental.model.trip.completion.TripResult
 
 class BikeAnchorage(val trips: TripRegistry, bikeShop: BikeShop = new BikeShop()) {
@@ -18,23 +18,15 @@ class BikeAnchorage(val trips: TripRegistry, bikeShop: BikeShop = new BikeShop()
     trips.finishCurrentTripForBike(bike)
   }
 
-  /**
-   * Given a {@link Token}, consumes the token and releases the {@link Bike}.
-   *
-   * @param token the RentToken.
-   * @return the Bike if present and reservedToken is OK, None otherwise.
-   * @throws IllegalStateException if the owner of the RentToken is banned
-   */
-  def releaseBike(token: Token): Option[Bike] = {
-    token match {
-      case reservedToken@ReservedRentToken(_, owner, _) =>
-        require(!owner.isBanned, "The user is banned.")
-        releaseParkedBikeIf(parked => !bikeShop.maintenancePickupRequests.exists(_.bike == parked))
-          .map(bike => trips.startTrip(bike, reservedToken))
-          .map(_.pickUp.bike)
-      case BikeMaintenanceToken(bike) =>
-        releaseParkedBikeIf(parked => parked == bike)
-    }
+  def releaseBike(token: ReservedRentToken): Option[Bike] = {
+    require(!token.owner.isBanned, "The user is banned.")
+    releaseParkedBikeIf(parked => !bikeShop.maintenancePickupRequests.exists(_.bike == parked))
+      .map(bike => trips.startTrip(bike, token))
+      .map(_.pickUp.bike)
+  }
+
+  def releaseBike(token: BikeMaintenanceToken): Option[Bike] = {
+    releaseParkedBikeIf(parked => parked == token.bike)
   }
 
   def requestBikeMaintenance(): Option[BikeMaintenanceRequest] =
