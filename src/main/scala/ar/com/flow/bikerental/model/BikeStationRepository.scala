90df22b57fa76e80d0d2009285a7f5ec16f0ab2a
Emiliano Menéndez <emystein@gmail.com>
2020-06-24 22:05:28 -0300

    BikeStationController.create

--- src/main/scala/ar/com/flow/bikerental/model/BikeStationRepository.scala
+++ src/main/scala/ar/com/flow/bikerental/model/BikeStationRepository.scala
@@ -1,5 +1,6 @@
 package ar.com.flow.bikerental.model
 
 trait BikeStationRepository {
+  def save(bikeStation: BikeStation): Unit
   def getById(stationId: String): Option[BikeStation]
 }
--- src/main/scala/ar/com/flow/bikerental/model/InMemoryBikeStationRepository.scala
+++ src/main/scala/ar/com/flow/bikerental/model/InMemoryBikeStationRepository.scala
@@ -5,5 +5,7 @@ import scala.collection.mutable
 class InMemoryBikeStationRepository extends BikeStationRepository {
   val stationsById: mutable.Map[String, BikeStation] = new mutable.HashMap()
 
+  override def save(bikeStation: BikeStation): Unit = stationsById.put(bikeStation.id, bikeStation)
+
   override def getById(stationId: String): Option[BikeStation] = stationsById.get(stationId)
 }
