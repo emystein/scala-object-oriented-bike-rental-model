3911e5a7493229921a2374b8e9e20f204e98e392
Emiliano Menéndez <emystein@gmail.com>
2020-06-27 23:44:54 -0300

    Minor cleanup.

--- src/main/scala/ar/com/flow/bikerental/model/BikeStation.scala
+++ src/main/scala/ar/com/flow/bikerental/model/BikeStation.scala
@@ -3,17 +3,20 @@ package ar.com.flow.bikerental.model
 import ar.com.flow.bikerental.model.token.ReservedRentToken
 
 case class BikeStation(id: Option[String], numberOfBikeAnchorages: Int, trips: TripRegistry, bikeShop: BikeShop) {
-  val bikeAnchorages: IndexedSeq[BikeAnchorage]  = (1 to numberOfBikeAnchorages).map(i => new BikeAnchorage(trips, bikeShop))
+  val bikeAnchorages: IndexedSeq[BikeAnchorage] =
+    (1 to numberOfBikeAnchorages).map(i => new BikeAnchorage(trips, bikeShop))
 
-  def getFreeSpots: Seq[BikeAnchorage] = bikeAnchorages.filter(anchorage => anchorage.parkedBike.isEmpty)
+  def getFreeSpots: Seq[BikeAnchorage] = bikeAnchorages.filter(_.parkedBike.isEmpty)
 
-  def pickupAvailableBike(reservedToken: ReservedRentToken): Option[Bike] = getAnyBikeAnchorageWithParkedBike.flatMap((anchorage: BikeAnchorage) => anchorage.releaseBike(reservedToken))
+  def pickupAvailableBike(reservedToken: ReservedRentToken): Option[Bike] =
+    getAnyBikeAnchorageWithParkedBike.flatMap(_.releaseBike(reservedToken))
 
-  def getAnyBikeAnchorageWithParkedBike: Option[BikeAnchorage] = bikeAnchorages.find(anchorage => anchorage.parkedBike.isDefined)
+  def getAnyBikeAnchorageWithParkedBike: Option[BikeAnchorage] =
+    bikeAnchorages.find(_.parkedBike.isDefined)
 
-  def getAnchorageById(anchorageId: Int): Option[BikeAnchorage] = {
+  def getAnchorageById(anchorageId: Int): Option[BikeAnchorage] =
     bikeAnchorages.drop(anchorageId - 1).headOption
-  }
 
-  def getParkedBikes: Seq[BikeAnchorage] = bikeAnchorages.filter(_.parkedBike.isDefined)
+  def getParkedBikes: Seq[BikeAnchorage] =
+    bikeAnchorages.filter(_.parkedBike.isDefined)
 }
