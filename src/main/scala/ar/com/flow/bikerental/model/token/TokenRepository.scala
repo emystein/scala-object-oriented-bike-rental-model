b84cee1246a678cfd2a4d0cb4e8dbc11850fcee8
Emiliano Menéndez <emystein@gmail.com>
2020-06-28 11:34:25 -0300

    Generalized TokenRepository from ReservedRentTokenRepository ConsumedRentTokenRepository.

---
+++ src/main/scala/ar/com/flow/bikerental/model/token/TokenRepository.scala
@@ -0,0 +1,16 @@
+package ar.com.flow.bikerental.model.token
+
+import ar.com.flow.bikerental.model.User
+
+trait TokenRepository[T] {
+  def save(token: T)
+  def getAll(): Iterable[T]
+  def getById(tokenId: String): Option[T]
+  def getAllByUser(user: User): Iterable[T]
+  def contains(token: T): Boolean
+  def deleteAll(): Unit
+}
+
+trait ReservedRentTokenRepository extends TokenRepository[ReservedRentToken]
+
+trait ConsumedRentTokenRepository extends TokenRepository[ConsumedRentToken]
