4269c4022b907e5920804b9c1d34d03e0215d610
Emiliano Menéndez <emystein@gmail.com>
2020-06-28 12:59:04 -0300

    Moved TokenRepository declaration to Token.scala.

--- src/main/scala/ar/com/flow/bikerental/model/token/Token.scala
+++ src/main/scala/ar/com/flow/bikerental/model/token/Token.scala
@@ -31,3 +31,12 @@ class ConsumedRentToken(val token: ReservedRentToken, val consumedAt: LocalDateT
     value.hashCode
   }
 }
+
+trait TokenRepository[T] {
+  def save(token: T)
+  def getAll(): Iterable[T]
+  def getById(tokenId: String): Option[T]
+  def getAllByUser(user: User): Iterable[T]
+  def contains(token: T): Boolean
+  def clear(): Unit
+}
