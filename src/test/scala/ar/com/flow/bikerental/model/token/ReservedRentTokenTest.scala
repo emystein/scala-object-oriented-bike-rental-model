45ca13cdba84c31232a21fed0fa0504412c49350
Emiliano Menéndez <emystein@gmail.com>
2020-09-28 23:57:11 -0300

    Convert ReservedRentTokenTest to WordSpec.

--- src/test/scala/ar/com/flow/bikerental/model/token/ReservedRentTokenTest.scala
+++ src/test/scala/ar/com/flow/bikerental/model/token/ReservedRentTokenTest.scala
@@ -4,33 +4,34 @@ import java.time.LocalDateTime
 
 import ar.com.flow.bikerental.model.TestObjects
 import org.scalatest.BeforeAndAfterEach
-import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
 
-class ReservedRentTokenTest extends AnyFunSuite with TestObjects with BeforeAndAfterEach with Matchers {
+class ReservedRentTokenTest extends AnyWordSpec with TestObjects with BeforeAndAfterEach with Matchers {
   override protected def beforeEach(): Unit = {
     tokenRegistry.clear()
   }
 
-  test("givenATransactionTokenWithExpirationDateInTheFutureWhenValidatingItThenItShouldBeValid") {
-    val notExpiredToken = new ReservedRentToken(value = "1", expiration = LocalDateTime.now.plusDays(1), owner = user)
-    notExpiredToken.hasExpired shouldBe false
-  }
-
-  test("givenATransactionTokenWithExpirationDateInThePastWhenValidatingItThenItShouldBeInvalid") {
-    val expiredToken = new ReservedRentToken(value = "1", expiration = LocalDateTime.now.minusDays(1), owner = user)
-    expiredToken.hasExpired shouldBe true
-  }
-
-  test("givenAReservedTokenWhenConsumeThenAConsumedTokenShouldBeCreated") {
-    val reservedToken = ReservedRentToken(expiration = LocalDateTime.now.plusDays(1), owner = user)
-    val consumedToken = tokenRegistry.consumeToken(reservedToken)
-    consumedToken shouldNot be(null)
-  }
-
-  test("givenATokenRegistryAndAReservedTokenWhenConsumeThenAConsumedTokenShouldBeRegisteredInTheTokenRegistry") {
-    val reservedToken = ReservedRentToken(expiration = LocalDateTime.now.plusDays(1), owner = user)
-    val consumedToken = tokenRegistry.consumeToken(reservedToken)
-    tokenRegistry.consumedTokens.getAll() should contain(consumedToken)
+  "ReservedRentToken" when {
+    "has expiration date in the future" should {
+      "be valid" in {
+        val notExpiredToken = ReservedRentToken(value = "1", expiration = LocalDateTime.now.plusDays(1), owner = user)
+        notExpiredToken.hasExpired shouldBe false
+      }
+    }
+    "has expiration date in the past" should {
+      "be valid" in {
+        val expiredToken = ReservedRentToken(value = "1", expiration = LocalDateTime.now.minusDays(1), owner = user)
+        expiredToken.hasExpired shouldBe true
+      }
+    }
+    "consumed" should {
+      "add a ConsumedRentToken to the TokenRegistry" in {
+        val reservedToken = ReservedRentToken(expiration = LocalDateTime.now.plusDays(1), owner = user)
+        val consumedToken = tokenRegistry.consumeToken(reservedToken)
+        consumedToken shouldNot be(null)
+        tokenRegistry.consumedTokens.getAll() should contain(consumedToken)
+      }
+    }
   }
 }
